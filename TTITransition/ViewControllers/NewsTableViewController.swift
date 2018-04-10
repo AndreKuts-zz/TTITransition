ce6177a3c63ca5399762224796eba89e63bd144c
Andrew Kuts <kuts.andre@gmail.com>
2018-04-10 18:12:34 +0300

    finalized API service adding a spinner

--- TTITransition/ViewControllers/NewsTableViewController.swift
+++ TTITransition/ViewControllers/NewsTableViewController.swift
@@ -24,6 +24,7 @@ class NewsTableViewController: UIViewController, UITableViewDataSource, UITableV
                 self.tableView.reloadData()
                 self.switchLoadIndicator()
             }
+            
         }
     }
     
